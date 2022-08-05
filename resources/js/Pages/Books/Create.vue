7c3ab070265237c9fd3a343b972f24fe199cb462
Yusniel Brocard <brocard@gmail.com>
2022-08-04 21:09:01 -0500

    first commit

---
+++ resources/js/Pages/Books/Create.vue
@@ -0,0 +1,16 @@
+<script setup>
+import BreezeGuestLayout from '@/Layouts/Guest.vue';
+</script>
+
+<template>
+    <BreezeGuestLayout>
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    </BreezeGuestLayout>
+</template>
+
+
+<style scoped>
+
+</style>
